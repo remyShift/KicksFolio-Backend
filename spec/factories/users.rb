1149e211745b40d0d3d60e7fc38ed9b979e2ab4d
Remy Cassagne <remy.engassac@icloud.com>
2024-12-14 21:00:38 +0100

    create users and sneakers factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { "password" }
+    first_name { "John" }
+    last_name { "Doe" }
+    pseudo { "johndoe" }
+    sneaker_size { 9 }
   end
 end
