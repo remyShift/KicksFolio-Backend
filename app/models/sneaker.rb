a12e4c7e5aed2f029a50433248601542646de34d
Remy Cassagne <remy.engassac@icloud.com>
2024-12-31 03:46:35 +0100

    add validates to status

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -2,7 +2,17 @@ class Sneaker < ApplicationRecord
   belongs_to :collection
   has_many_attached :images
 
-  validates :brand, :model, :size, :condition, presence: true
+  STATUS = ['rocking', 'stocking', 'selling', 'other']
+
+  validates :brand, :model, :size, :condition, :status, presence: true
   validates :collection_id, presence: true
   validates :images, presence: true
+
+  validate :status_inclusion
+
+  def status_inclusion
+    unless STATUS.include?(status)
+      errors.add(:status, "is not a valid status")
+    end
+  end
 end
